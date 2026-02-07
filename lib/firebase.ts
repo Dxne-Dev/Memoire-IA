0eddbac38df98ec818e317e709694e8d5aad775e
Dxne-Dev <alassanemomo244@gmail.com>
2026-02-07 22:55:25 +0100

    security: move firebase config to environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA54v_TLKTKP_jf-K-v2bDFBIjmRQDinqE",
-    authDomain: "memoire-ia-88600.firebaseapp.com",
-    projectId: "memoire-ia-88600",
-    storageBucket: "memoire-ia-88600.firebasestorage.app",
-    messagingSenderId: "624532746053",
-    appId: "1:624532746053:web:8aef323b542aa71d70182c"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase (singleton pattern to avoid reloading errors in Next.js hot reload)
